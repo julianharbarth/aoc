4bcf5e17813ed0b6c3cb4f31e9b09a7e37ca8274
julian <julian@harbarth.info>
2021-12-03 23:49:41 +0100

    move aoc1.rs to src and add aoc.rs

---
+++ src/aoc.rs
@@ -0,0 +1,5 @@
+pub mod aoc1;
+pub mod aoc2;
+
+pub use aoc1::aoc1;
+pub use aoc2::aoc2;
--- src/aoc/aoc1.rs
+++ src/aoc/aoc1.rs
@@ -1,5 +1,5 @@
-fn main() {
-  let collected = std::fs::read_to_string("input.txt")
+pub fn aoc1(path: &std::path::Path) -> (i32, i32) {
+  let collected = std::fs::read_to_string(path)
     .unwrap()
     .lines()
     .map(|s| s.parse::<i32>().unwrap())
@@ -8,12 +8,12 @@ fn main() {
   let part_one = collected
     .windows(2)
     .fold(0, |acc, p| acc + ((p[0] < p[1]) as i32) );
-  println!("Part One {}", part_one);
 
   let part_two = collected
     .windows(3)
     .zip(collected.windows(3).skip(1))
     .fold(0, |acc, (p1, p2) |
       acc + ((p1.iter().sum::<i32>() < p2.iter().sum::<i32>()) as i32) );
-  println!("Part two {}", part_two);
+
+  (part_one, part_two)
 }
